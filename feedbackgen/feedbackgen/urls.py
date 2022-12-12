9f1cbc9c3b66306f28589db96821fd2bf0093207
FailedRift <ikennaaokon@gmail.com>
2022-12-12 20:34:37 +0100

    Views and urls updated

--- feedbackgen/feedbackgen/urls.py
+++ feedbackgen/feedbackgen/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dashboard/', include('core.urls')),
+    path('feedback/', include('core.urls')),
 ]
