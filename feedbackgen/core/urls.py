d857becedee3e605f124d096c14fcd94e46d7b3b
Iloabuchi-Collins <iloabuchicollins1000@gmail.com>
2022-12-13 21:36:12 +0100

    Added authenticaton views and form

--- feedbackgen/core/urls.py
+++ feedbackgen/core/urls.py
@@ -5,10 +5,10 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
     path('login/', views.login_view, name='login'),
     #path('feedback', views.feedback, name='feedback'),
+    path('signup/', views.signup_view, name='signup'),
     path('faq/', views.faq, name='faq'),
     path('pricing/', views.pricing, name='pricing')
 ]
